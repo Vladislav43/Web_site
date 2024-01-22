b1a4efaea8ae4f513fbeb289c1bc9505ca8f99d3
Vladislav43 <92948301+Vladislav43@users.noreply.github.com>
2024-01-22 19:35:36 +0200

    fix

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
 
 mongoose.connect('mongodb+srv://voievoda:orewit123.@cluster0.hjg23dj.mongodb.net/?retryWrites=true&w=majority')
 .then(()=> console.log('db ok'))
@@ -14,16 +17,15 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.post('/auth/login', (req, res) => {
-  
-  const token = jwt.sign({
-    email: req.body.email,
-  }, 'hash');
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
   res.json({
     success: true,
-    token: token, 
-  });
+  })
 });
 
 app.listen(port, function (err) {
@@ -32,4 +34,5 @@ app.listen(port, function (err) {
   }
 
   console.log(`Server is running on port ${port}`);
+
 });
